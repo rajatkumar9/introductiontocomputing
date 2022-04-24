ab3480181215ed2ef9eb62d145e5ebdb14b1cba5
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-04-24 19:39:18 +0530

    Add files via upload

---
+++ pythonassignmentoutput.py
@@ -0,0 +1,34 @@
+Enter number 1 :3
+Enter number 2:4
+Enter number 3:5
+The average is: 4.0
+Enter your income in $:100000
+Enter number of dependents in your family:2
+tax on your income is: 16800.0
+Enter number of seconds : 362
+It has 6 minutes and  2 seconds
+75.0 <class 'str'>
+cosine( 0 ) is 1.0            sine( 0 ) is 0.0
+cosine( 15 ) is 0.9659            sine( 15 ) is 0.2588
+cosine( 30 ) is 0.866            sine( 30 ) is 0.5
+cosine( 45 ) is 0.7071            sine( 45 ) is 0.7071
+cosine( 60 ) is 0.5            sine( 60 ) is 0.866
+cosine( 75 ) is 0.2588            sine( 75 ) is 0.9659
+cosine( 90 ) is 0.0            sine( 90 ) is 1.0
+cosine( 105 ) is -0.2588            sine( 105 ) is 0.9659
+cosine( 120 ) is -0.5            sine( 120 ) is 0.866
+cosine( 135 ) is -0.7071            sine( 135 ) is 0.7071
+cosine( 150 ) is -0.866            sine( 150 ) is 0.5
+cosine( 165 ) is -0.9659            sine( 165 ) is 0.2588
+cosine( 180 ) is -1.0            sine( 180 ) is 0.0
+cosine( 195 ) is -0.9659            sine( 195 ) is -0.2588
+cosine( 210 ) is -0.866            sine( 210 ) is -0.5
+cosine( 225 ) is -0.7071            sine( 225 ) is -0.7071
+cosine( 240 ) is -0.5            sine( 240 ) is -0.866
+cosine( 255 ) is -0.2588            sine( 255 ) is -0.9659
+cosine( 270 ) is -0.0            sine( 270 ) is -1.0
+cosine( 285 ) is 0.2588            sine( 285 ) is -0.9659
+cosine( 300 ) is 0.5            sine( 300 ) is -0.866
+cosine( 315 ) is 0.7071            sine( 315 ) is -0.7071
+cosine( 330 ) is 0.866            sine( 330 ) is -0.5
+cosine( 345 ) is 0.9659            sine( 345 ) is -0.2588
