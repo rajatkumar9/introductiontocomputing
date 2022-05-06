8f479bf5c71d784b05920ad6bec931d199d38075
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-05-07 00:08:58 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+
+The length of given string is :  35
+The reverse of string is: egaugnal evitisnes esac a si nohtyP
+new string using slice operation : a case sensitive
+Python is object oriented language
+indexes of a are : 10 13 28
+string after replacing blank spaces : Pythonisacasesensitivelanguage
+Hey Rajat Here!
+My SID is 21107115
+Iam from Mechanical department and my CGPA is: 9.9
+A. a & b = 8
+B. a | b = 58
+C. a ^ b = 50
+D. a << 2 =  224
+E. a >> 2 =  14  and  b >> 4 =  0
+Enter a string :my name is
+yes
+Enter First length : 3
+Enter Second length : 4
+Enter Third length : 5
+The triangle can be formed?
+Yes!
+enter number 1:3
+enter number 2:7
+1
