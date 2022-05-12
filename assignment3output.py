44e3e22125df0ad22cdf602692a98ffb2910daec
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-05-12 22:40:02 +0530

    Add files via upload

---
+++ assignment3output.py
@@ -0,0 +1,70 @@
+Enter a number6
+Binary equivalent of 6 is 110
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+5.exponent
+Enter choice(1/2/3/4/5): 5
+Enter first number: 3
+Enter second number: 2
+3.0 ^ 2.0 = 9.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4/5): 1
+Enter first number: 3
+Enter second number: 5
+3.0 + 5.0 = 8.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4/5): 2
+Enter first number: 3
+Enter second number: 5
+3.0 - 5.0 = -2.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4/5): 3
+Enter first number: 3
+Enter second number: 4
+3.0 * 4.0 = 12.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4/5): 4
+Enter first number: 6
+Enter second number: 5
+6.0 / 5.0 = 1.2
+Let's do next calculation? (yes/no): no
+(3+4)*5 is 35
+enter value of n :3
+3.0
+enter value of r :4
+201.06192982974676
+enter value of angle 1 (a):45
+enter value of angle 2 (b) :45
+2.0
+numbersin range(5)
+0
+1
+2
+3
+4
+numbersin range(3,10)
+3
+4
+5
+6
+7
+8
+9
+numbersin range(4,13,3)
+4
+7
+10
+numbersin range(15,5,-2)
+15
+13
+11
+9
+7
+numbersin range(5,3)
+Enter number of carbon:3
+Enter number of hydrogen:2
+Enter number of oxygen:0
+molecular weight of compound in grams per mole= 38.04798
