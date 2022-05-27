5b7d4b054a3b08d48f6761c21c403c9b397318df
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-05-27 21:57:43 +0530

    Add files via upload

---
+++ assignment4output.py
@@ -0,0 +1,14 @@
+Enter your marks : 50
+Your grade is D
+Enter a Year :2000
+The entered year is a leap year
+Q1 is: 1x10 =
+10
+Right!
+Q2 is: 10x2 =
+20
+Right!
+Q3 is: 7x2 =
+4
+Wrong!
+There are 177 candies in total
