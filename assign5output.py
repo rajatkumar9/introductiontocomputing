dbbc6c5f5bbe8d114b9379ce20dbadd353865c2a
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-06-15 23:15:20 +0530

    Add files via upload

---
+++ assign5output.py
@@ -0,0 +1,133 @@
+enter the string:python
+The reverse of string is: nohtyp
+starting of range:2
+ending of range:100
+enter number to check divisiblity:6
+6
+12
+18
+24
+30
+36
+42
+48
+54
+60
+66
+72
+78
+84
+90
+96
+Enter length of side one :3
+Enter length of side two :4
+Enter length of side three :5
+Area of given triangle 6.0
+*
+
+**
+
+***
+
+****
+
+*****
+
+****
+
+***
+
+**
+
+*
+
+A
+
+BC
+
+DEF
+
+GHIJ
+
+KLMNO
+
+Enter strating of range2
+Enter ending of range100
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+QUESTION 7
+77
+154
+231
+308
+385
+462
+Enter the 10 no. for list
+Enter the 1 no. for list
+3
+Enter the 2 no. for list
+2
+Enter the 3 no. for list
+-4
+Enter the 4 no. for list
+5
+Enter the 5 no. for list
+6
+Enter the 6 no. for list
+7
+Enter the 7 no. for list
+2
+Enter the 8 no. for list
+-4
+Enter the 9 no. for list
+3
+Enter the 10 no. for list
+6
+list entered [3, 2, -4, 5, 6, 7, 2, -4, 3, 6]
+positive no.
+ {2, 3, 5, 6, 7}
+negative no.
+ {-4}
+odd no.
+ {3, 5, 7}
+even no.
+ {2, -4, 6}
+No. of occurrence is {3: 2, 2: 2, -4: 2, 5: 1, 6: 2, 7: 1}
+Enter total elements in the list7
+Enter a word run
+Enter a word python
+Enter a word pc
+Enter a word programme
+Enter a word yt
+Enter a word vt
+Enter a word xz
+run occurs 1 times
+python occurs 1 times
+pc occurs 1 times
+programme occurs 1 times
+yt occurs 1 times
+vt occurs 1 times
+xz occurs 1 times
