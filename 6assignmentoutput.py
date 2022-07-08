7beb3b553343ba0df7b5ad4b91c1afe4a27cdf30
rajatkumar9 <104300438+rajatkumar9@users.noreply.github.com>
2022-07-08 22:56:45 +0530

    Add files via upload

---
+++ 6assignmentoutput.py
@@ -0,0 +1,41 @@
+Q1
+Enter a number to check if it is perfect:56
+56 is not a perfect number
+Q2
+Enter a word, phrase or sequence to see if it is a palindrome:madam
+It is a palindrome.
+Q3
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+Q4
+Enter a string to check if it is a pangram: python is a programming language
+It is not a pangram.
+Q5
+Enter a hyphen-separated sequence of words: c-a-f-d-b-e
+a-b-c-d-e-f
+Q6
+
+Student ID: 21107115
+Student Name: albert
+
+Student ID: 21107116
+Student Name: Henry
+
+Student Name: Henry
+Student Class: 12
+Q7
+<__main__.Student object at 0x000002414ABB87C0>
+rajat
+<__main__.Marks object at 0x000002414ABB8820>
+75
+86
+class Student: <class 'type'> class Marks: <class 'type'>
+Q8
+[[-10, 2, 8], [-7, -3, 10]]
+Q9
+True
+False
+True
